b51c193c413e880853c21e44f54e0d2c71bf3557
Mohamed Ali <maellleyyy@gmail.com>
2021-05-25 22:27:19 +0500

    completed

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,19 +1,93 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+const STYLES = {
+  small: {
+    fontSize: 14 / 16,
+    iconSize: 16,
+    borderThickness: 1,
+    height: 24,
+    paddingLeft: 24,
+  },
+  large: {
+    fontSize: 18 / 16,
+    iconSize: 24,
+    borderThickness: 2,
+    height: 36,
+    paddingLeft: 32,
+  },
 };
+
+const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+  const styles = STYLES[size];
+
+  // TODO: VAlidate styles for error
+
+  return (
+    <Wrapper>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <Ico
+        id={icon}
+        size={styles.iconSize}
+        strokeWidth={styles.borderThickness}
+        style={{ "--size": styles.iconSize + "px" }}
+      />
+      <Input
+        style={{
+          "--width": width + "px",
+          "--height": styles.height + "px",
+          "--border-thickness": styles.borderThickness + "px",
+          "--font-size": styles.fontSize + "rem",
+          "--padding-left": styles.paddingLeft + "px",
+        }}
+        placeholder={placeholder}
+        type="text"
+        aria-label={label}
+      />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.label`
+  display: block;
+  position: relative;
+  width: max-content;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const Ico = styled(Icon)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  margin: auto;
+`;
+
+const Input = styled.input`
+  width: var(--width);
+  height: var(--height);
+  font-size: var(--font-size);
+  border: none;
+  border-bottom: var(--border-thickness) solid ${COLORS.black};
+  padding: 4px;
+  padding-left: var(--padding-left);
+  color: inherit;
+  font-weight: 700;
+  outline-offset: 2px;
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
+`;
 
 export default IconInput;
--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,57 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const SIZES = {
+  large: {
+    "--padding": "4px",
+    "--height": "16px",
+  },
+  medium: {
+    "--padding": "0",
+    "--height": "12px",
+  },
+  small: {
+    "--padding": "0",
+    "--height": "8px",
+  },
+};
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const styles = SIZES[size];
+
+  return (
+    <Wrapper style={styles}>
+      <Bar
+        role="progressbar"
+        aria-valuenow={value}
+        aria-valuemin="0"
+        aria-valuemax="100"
+      >
+        <VisuallyHidden>{value}</VisuallyHidden>
+      </Bar>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  padding: var(--padding);
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  /* To hide the UI issue that floats when progressbar reach 99% */
+  overflow: hidden;
+`;
+
+const Bar = styled.div`
+  background-color: ${COLORS.primary};
+  width: ${(props) => props["aria-valuenow"] + "%"};
+  height: var(--height);
+  border-radius: ${(props) =>
+    props["aria-valuenow"] === 100 ? "4px" : "4px 0 0 4px"};
+`;
 
 export default ProgressBar;
